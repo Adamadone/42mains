05bb0c093be9bf4edd51ebac4ce46c5e3d5d1d98
Adam Zacios <azacios@c3r3s1.42prague.com>
2022-10-24 16:48:34 +0200

    First iteration of the c04 main.c file

---
+++ c04_main.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <string.h>
+#include <unistd.h>
+
+//#include "ex00/ft_strlen.c"
+//#include "ex01/ft_putstr.c"
+//#include "ex02/ft_putnbr.c"
+#include "ex03/ft_atoi.c"
+//#include "ex04/ft_putnbr_base.c"
+//#include "ex05/ft_atoi_base.c"
+
+int	main()
+{
+	//00 Return value should be the legth of the string
+	//printf("String legth: <%d> \n", ft_strlen("I like bananas and pineapples"));
+
+	//01 Return value should be the string itself
+	//ft_putstr("Understandable, have a nice day");
+
+	//02 Return value should be any number in the int range
+	/*
+	ft_putnbr(-2147483648);
+	write(1, "\n", 1);
+	ft_putnbr(2147483647);
+	write(1, "\n", 1);
+	ft_putnbr(0);
+	write(1, "\n", 1);
+	ft_putnbr(5);
+	write(1, "\n", 1);
+	*/
+
+	//03 Return value should be the first chain of numbers
+	//printf("Atoied string: <%d>",  ft_atoi("  	++---7a89456sasa"));
+
+	//04
+	return (0);
+}
+
