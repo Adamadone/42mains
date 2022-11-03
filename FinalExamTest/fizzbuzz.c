4f513d5cdaa4d0e273edf65999b4c40e7974acfd
Adam Zacios <azacios@c3r3s1.42prague.com>
2022-11-03 14:16:46 +0100

    First commit of all of my shit

---
+++ FinalExamTest/fizzbuzz.c
@@ -0,0 +1,29 @@
+#include <unistd.h>
+
+int ft_is_div3(int i)
+{
+	if ((i % 3) == 0)
+		write (1, "fizz", 4);
+	if (())
+}
+
+
+
+
+
+void	main(void)
+{
+	int i;
+
+	i = 0;
+	while (i != 100)
+	{
+		if ((i % 3) == 0)
+			write (1, "fizz", 4);
+			break ;
+		else if ((i % 5) == 0)
+			write (1, "buzz", 4);
+		else
+			write (1, &i, 1);
+	}
+}
